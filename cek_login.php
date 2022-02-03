ed1323ff70d40da1f47657bb2d36faff0b18989b
Astri Musidah <astry.musidah@gmail.com>
2022-02-03 22:58:14 +0700

    menghubungkan repo lokal dan repo github

---
+++ cek_login.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+include 'koneksi.php';
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+
+$data = mysqli_query($koneksi, "select * from user_astri where username='$username' and password='$password'");
+
+$cek = mysqli_num_rows($data);
+
+if($cek > 0){
+    $_SESSION['username'] = $username;
+    $_SESSION['status'] = "login";
+    header("location:index.php");
+}else{
+    header("location:login.php?pesan=gagal");
+}
